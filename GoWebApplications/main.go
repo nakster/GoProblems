3925b6b180cdab35d086234d9ac5110d7f661766
NAQI AHMAD - Student <G00332403@gmit.ie>
2017-10-27 15:31:49 +0100

    Question 5 and 6

--- GoWebApplications/main.go
+++ GoWebApplications/main.go
@@ -5,10 +5,16 @@ import (
 	"net/http"
 	"html/template"
 	"log"
+	"strconv"
+	"time"
+	"math/rand"
+	"fmt"
 )
 type messagStruct struct{
 
 	Message string
+	Guess int
+	newMessage string
 
 }
 //can name it anything you want i.e handlerFunc
@@ -18,9 +24,36 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 }
 //can name it anything you want i.e handlerFunc 
 func handleGuess(w http.ResponseWriter, r *http.Request) {
+
+	randomNum := getRandumNum(1, 20)
+	//set a new cookie if doesnt exists 
+	cookie, err := r.Cookie("target"); 
+
+	if err != nil {
+		cookie = &http.Cookie{Name: "target", Value: strconv.Itoa(randomNum),Expires: time.Now().Add(72 * time.Hour),
+
+		}
+		http.SetCookie(w, cookie)
+
+	}
+
+	fmt.Println(cookie)
+
+	//assign values 
+	gues, _ := strconv.Atoi(r.FormValue("guess"))
+
 	//create a new variable and assign the struct and string value
 	 mess := &messagStruct{
-		 Message: "Guess a number between 1 and 20"}
+		 Message: "Guess a number between 1 and 20", Guess: gues}
+
+
+	cookValue, _ := strconv.Atoi(cookie.Value)
+
+	//if the target is = to guess then declare the winner and change the target
+	if cookValue == gues{
+
+		
+	}
 	
 	 //this parses the html file 
 	t, err := template.ParseFiles("guess.tmpl") 
@@ -33,6 +66,13 @@ func handleGuess(w http.ResponseWriter, r *http.Request) {
     if err != nil { 
   	  log.Print("error ", err) 
   	}
+}
+
+//random number generator 
+func getRandumNum(min, max int) int{
+// Random int will be different each program execution.
+	rand.Seed( time.Now().UnixNano())
+   	return rand.Intn(max - min) + min
 }
 
 func main() {
