decdc3dbac8c98c222266f89f4ed070580f5513e
NAQI AHMAD - Student <G00332403@gmit.ie>
2017-10-27 14:31:12 +0100

    Question 3

--- GoWebApplications/main.go
+++ GoWebApplications/main.go
@@ -1,22 +1,18 @@
 package main
 
 import (
-	"fmt"
+	//"fmt"
 	"net/http"
 )
 
 //can name it anything you want i.e handlerFunc
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Guessing game</h1>")
+	http.ServeFile(w, r, "index.html")
 }
 
-func handlerFunc2(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>New Game</h1>")
-}
 
 func main() {
 	///////////open localhost on port 8080
 	http.HandleFunc("/", handlerFunc)
-	http.HandleFunc("/guess", handlerFunc2)
 	http.ListenAndServe(":8080", nil)
 }
